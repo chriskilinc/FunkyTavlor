56ee32a1b7b123552d64e2d888e8b7bea117159e
Filimix <wobzar@hotmail.se>
2016-02-04 13:35:39 +0100

    Pull this daniil

--- ProjectArtStone/ProjectArtStone/uploadimagejamwindow2.xaml.cs
+++ ProjectArtStone/ProjectArtStone/uploadimagejamwindow2.xaml.cs
@@ -63,37 +63,86 @@ namespace ProjectArtStone
             sqlCon.Close();
 
 
+
+
             byte[] data = (byte[])ds.Tables[0].Rows[0][0];
 
             MemoryStream strm = new MemoryStream();
 
-            using (MemoryStream stream = strm)
-            {
-                var imageSource = new BitmapImage();
-                imageSource.BeginInit();
-                imageSource.StreamSource = stream;
+            System.Drawing.Image imgWinForms = System.Drawing.Image.FromStream(strm);
 
-                strm.Write(data, 0, data.Length);
 
-                strm.Position = 0;
-                                                                 //1
 
-                BitmapImage bi = new BitmapImage();
+            strm.Write(data, 0, data.Length);
 
-                bi.BeginInit();
+            strm.Position = 0;
 
-                MemoryStream ms = new MemoryStream();
 
-                                                                   //2 
+            // ImageSource ...
 
-                ms.Seek(0, SeekOrigin.Begin);
+            BitmapImage bi = new BitmapImage();
 
-                bi.StreamSource = ms;
+            bi.BeginInit();
 
-                bi.EndInit();
+            MemoryStream ms = new MemoryStream();
 
-                imagebox.Source = bi;
-            }
+            // Save to a memory stream...
+
+            imgWinForms.Save(ms, ImageFormat.Bmp);
+
+            // Rewind the stream...
+
+            ms.Seek(0, SeekOrigin.Begin);
+
+            // Tell the WPF image to use this stream...
+
+            bi.StreamSource = ms;
+
+            bi.EndInit();
+
+            imagebox.Source = bi;
+
+
+
+
+
+
+
+
+
+
+
+            //byte[] data = (byte[])ds.Tables[0].Rows[0][0];
+
+            //MemoryStream strm = new MemoryStream();
+
+            //using (MemoryStream stream = strm)
+            //{
+            //    var imageSource = new BitmapImage();
+            //    imageSource.BeginInit();
+            //    imageSource.StreamSource = stream;
+
+            //    strm.Write(data, 0, data.Length);
+
+            //    strm.Position = 0;
+            //                                                     //1
+
+            //    BitmapImage bi = new BitmapImage();
+
+            //    bi.BeginInit();
+
+            //    MemoryStream ms = new MemoryStream();
+
+            //                                                       //2 
+
+            //    ms.Seek(0, SeekOrigin.Begin);
+
+            //    bi.StreamSource = ms;
+
+            //    bi.EndInit();
+
+            //    imagebox.Source = bi;
+        }
 
              
             //imagebox.Source = imageSource;   1
