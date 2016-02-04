54a3c81b85846ca39b5d2de092f5aae8a8f0757d
Filimix <wobzar@hotmail.se>
2016-02-04 13:12:04 +0100

    Hello world, its still not working, same problem as yesturday

--- ProjectArtStone/ProjectArtStone/uploadimagejamwindow2.xaml.cs
+++ ProjectArtStone/ProjectArtStone/uploadimagejamwindow2.xaml.cs
@@ -13,8 +13,10 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Drawing;
+
 
 namespace ProjectArtStone
 {
@@ -61,36 +63,70 @@ namespace ProjectArtStone
             sqlCon.Close();
 
 
+            byte[] data = (byte[])ds.Tables[0].Rows[0][0];
 
-            //byte[] data = (byte[])ds.Tables[0].Rows[0][0];
+            MemoryStream strm = new MemoryStream();
 
-            //MemoryStream strm = new MemoryStream();
+            using (MemoryStream stream = strm)
+            {
+                var imageSource = new BitmapImage();
+                imageSource.BeginInit();
+                imageSource.StreamSource = stream;
 
-            //strm.Write(data, 0, data.Length);
+                strm.Write(data, 0, data.Length);
 
-            //strm.Position = 0;
+                strm.Position = 0;
+                                                                 //1
 
-            //Image img = Image.FromStream(strm);
+                BitmapImage bi = new BitmapImage();
 
-            ////Image img = Image.FromStream(strm);
+                bi.BeginInit();
 
-            //BitmapImage bi = new BitmapImage();
+                MemoryStream ms = new MemoryStream();
 
-            //bi.BeginInit();
+                                                                   //2 
 
-            //MemoryStream ms = new MemoryStream();
+                ms.Seek(0, SeekOrigin.Begin);
 
-            //img.Save(ms, Image.ImageFormat.Bmp);
+                bi.StreamSource = ms;
 
-            //ms.Seek(0, SeekOrigin.Begin);
+                bi.EndInit();
 
-            //bi.StreamSource = ms;
+                imagebox.Source = bi;
+            }
 
-            //bi.EndInit();
+             
+            //imagebox.Source = imageSource;   1
 
-            //imagebox.Source = bi;
+            //img.Save(ms, Image.ImageFormat.Bmp);   2
+
+
+
+
+            ////System.Drawing.Image img = System.Drawing.Image.FromStream(strm);
+
+
+            //  Image img; //= Image.FromStream(strm);
 
 
         }
+
+        private static BitmapImage LoadImage(Stream stream)
+        {
+            // assumes that the streams position is at the beginning
+            // for example if you use a memory stream you might need to point it to 0 first
+            var image = new BitmapImage();
+
+            image.BeginInit();
+            image.CacheOption = BitmapCacheOption.OnLoad;
+            image.StreamSource = stream;
+            image.EndInit();
+
+            image.Freeze();
+            return image;
+        }
+
+
+
     }
 }
