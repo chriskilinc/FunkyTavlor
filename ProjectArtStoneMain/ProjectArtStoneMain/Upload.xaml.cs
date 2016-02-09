d6f63352b0b0a00a48a292b31f2b7b51889f3e04
Chriskilinc <chriskilinc@gmail.com>
2016-02-09 14:15:17 +0100

    Working on upload btn

--- ProjectArtStoneMain/ProjectArtStoneMain/Upload.xaml.cs
+++ ProjectArtStoneMain/ProjectArtStoneMain/Upload.xaml.cs
@@ -24,6 +24,7 @@ namespace ProjectArtStoneMain
         {
             InitializeComponent();
         }
+        string bytedata;
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
@@ -36,7 +37,7 @@ namespace ProjectArtStoneMain
 
                 byte[] data = new byte[fs.Length];
                 fs.Read(data, 0, System.Convert.ToInt32(fs.Length));
-
+                
                 fs.Close();
 
                 //Additem
@@ -47,8 +48,19 @@ namespace ProjectArtStoneMain
                 image.SetValue(Image.SourceProperty, imgs.
                 ConvertFromString(dlg.FileName.ToString()));
 
-                textBox.Text = "";
+                bytedata = data.ToString();
+                label1.Content = "";
             }
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            OGArtworkDB.Artlist.Add(new OGArtwork() { ArtId = 1, Title = textBox.Text, Description = "Desctiprion", Rum = 0, Image = bytedata ,Visable = true });
+        }
+
+        public void CheckDB()
+        {
+            
         }
     }
 }
