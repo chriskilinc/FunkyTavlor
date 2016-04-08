9db665c3bee708f15fda3d0afc61fa10d48c9829
Chriskilinc <chriskilinc@gmail.com>
2016-04-08 13:22:55 +0200

    Added DefultStyle.css and reference in BundleConfig.cs

--- MvcArtStone/MvcArtStone/App_Start/BundleConfig.cs
+++ MvcArtStone/MvcArtStone/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace MvcArtStone
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css","~/Content/DefultStyle.css"));
         }
     }
 }
