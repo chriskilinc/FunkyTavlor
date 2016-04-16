3efb03eb94103db3a1af20185fa838a6b2784af0
Chriskilinc <chriskilinc@gmail.com>
2016-04-16 17:52:22 +0200

    Added artstoneApp.js and artstoneController.js. Added "saveArtwork" function in controller

---
+++ MvcArtStone/MvcArtStone/Scripts/Angular/artstoneController.js
@@ -0,0 +1,39 @@
+﻿angular.module('artstoneApp')
+.controller('artstoneController', ['$http', '$scope', function ($http, $scope) {
+
+    $scope.artwork = {
+        Title: '',
+        Artist: '',
+        Room: '',
+        Description: '',
+        InStorage: '',
+        CreationDate: '',
+        AddedDate: '',
+    }
+
+    $scope.saveArtwork = function (artwork) {
+        $http.post('/home/submit', artwork).then(function (response) {
+            console.log(response);
+        });
+    }
+
+    //$scope.company = {
+    //    CorporateIdentityNumber: '',
+    //    ContactPersonName: '',
+    //    CurrencyCode: '',
+    //    InvoiceAddress1: '',
+    //    InvoiceCity: '',
+    //    InvoiceCountryCode: '',
+    //    InvoicePostalCode: '',
+    //    Name: '',
+    //    TermsOfPaymentId: '',
+    //}
+
+    //$scope.saveCompany = function (company) {
+
+    //    $http.post('/home/submit', company).then(function (response) {
+    //        console.log(response)
+    //    });
+    //}
+
+}]);
