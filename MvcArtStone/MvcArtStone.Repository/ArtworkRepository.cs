3581b4b545d82218869dd6c02ef1466e09dfd573
Chriskilinc <chriskilinc@gmail.com>
2016-04-16 18:07:49 +0200

    Added MvcArtStone.Repository, DatabaseHelper.cs and ArtworkRepository.cs

---
+++ MvcArtStone/MvcArtStone.Repository/ArtworkRepository.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace MvcArtStone.Repository
+{
+    public class ArtworkRepository
+    {
+        private readonly DatabaseHelper _databaseHelper;
+
+        public ArtworkRepository()
+        {
+            _databaseHelper = new DatabaseHelper(); ;
+        }
+
+
+        //CRUD Artwork
+        //public void AddCompany(Models.Company company)
+        //{
+        //    using (var context = new GatewayContextDataContext(_databaseHelper.GetConnectionString()))
+        //    {
+        //        var newCompany = new Company
+        //        {
+        //            VATRegistrationNumber = company.CorporateIdentityNumber,
+        //            CompanyName = company.Name,
+        //            CompanyNameAlternative = company.Name,
+        //            WebAddress = "www.example.com",
+        //            Deleted = false,
+        //            RegistrationDate = DateTime.Now,
+        //            Comment = "Test Company",
+        //            Address = new Address
+        //            {
+        //                Street = company.InvoiceAddress1,
+        //                City = company.InvoiceCity,
+        //                ZipCode = company.InvoicePostalCode,
+        //                AddressType = new AddressType
+        //                {
+        //                    AddressTypeId = 1,  //Fakturaadress
+        //                },
+        //                Country = new Country
+        //                {
+        //                    CountryCode = company.InvoiceCountryCode,
+        //                    IsEU = true, //check if country is in EU
+        //                    EnglishName = "EnglishName",
+        //                    NativeName = "NativeName",
+        //                    CultureCode = "CultureCode",
+        //                    CallingCode = "CallingCode",
+        //                }
+        //            },
+        //            ContactPerson = new ContactPerson
+        //            {
+        //                FirstName = company.ContactPersonName
+
+        //            },
+        //            CompanyInvoiceSetting = new CompanyInvoiceSetting
+        //            {
+        //                Currency = new Currency
+        //                {
+        //                    CurrencyCode = company.CurrencyCode,
+        //                    CurrencyIsoCode = "s",
+        //                },
+        //                DueDays = 1,
+        //            },
+
+
+        //            //Add more fields
+        //        };
+
+        //        context.Companies.InsertOnSubmit(newCompany);
+        //        //context.Addresses.InsertOnSubmit(newCompany.Address);
+        //        //context.Companies..InsertOnSubmit(newCompany.VATRegistrationNumber);
+        //        context.SubmitChanges();
+        //    }
+        //}
+    }
+}
---
+++ MvcArtStone/MvcArtStone.Repository/DatabaseHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace MvcArtStone.Repository
+{
+    public class DatabaseHelper
+    {
+        private static string _connectionString;
+
+        public string GetConnectionString()
+        {
+            if (!string.IsNullOrEmpty(_connectionString))
+                return _connectionString;
+
+            _connectionString =
+                "";
+            return _connectionString;
+        }
+    }
+}
