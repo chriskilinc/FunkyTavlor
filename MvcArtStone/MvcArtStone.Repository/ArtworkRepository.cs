90da4d64e5c63eaddb3de9c598d974f388323a52
filimix <wobzar@hotmail.se>
2016-05-20 00:49:22 +0200

    usethisrepofile

--- MvcArtStone/MvcArtStone.Repository/ArtworkRepository.cs
+++ MvcArtStone/MvcArtStone.Repository/ArtworkRepository.cs
@@ -138,51 +138,46 @@ namespace MvcArtStone.Repository
             }
         }
 
-        public static Artwork EditArtworkByModel(Artwork model)
+        
+
+
+        public static void EditArtworkByModel(Artwork model)
         {
             CloudStorageAccount storageAccount = CloudStorageAccount.Parse(_databaseHelper.GetConnectionString());
 
             CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
 
-            CloudTable table;
-            table = tableClient.GetTableReference("funkytavlor");
+            CloudTable table = tableClient.GetTableReference("funkytavlor");
 
-            Artwork SingleArtworkEntity = null;
+            TableOperation retrieveOperation = TableOperation.Retrieve<Artwork>("ostra", model.Id.ToString());
 
-            // Create a retrieve operation that takes a customer entity.
-            if (model.Id != Guid.Empty)
+            TableResult retrievedResult = table.Execute(retrieveOperation);
+
+            Artwork editArtwork = (Artwork)retrievedResult.Result;
+
+            if (editArtwork != null)
             {
-                TableOperation retrieveOperation = TableOperation.Retrieve<Artwork>("ostra", model.Id.ToString());
+                editArtwork.Title = model.Title;
+                editArtwork.Artist = model.Artist;
+                editArtwork.Room = model.Room;
+                editArtwork.Signed = model.Signed;
+                editArtwork.Visible = model.Visible;
+                editArtwork.ImgUrl = "";
+                editArtwork.Description = model.Description;
+                editArtwork.InStorage = model.InStorage;
 
-                // Execute the operation.
-                TableResult retrievedResult = table.Execute(retrieveOperation);
+                TableOperation editOperation = TableOperation.Replace(editArtwork);
 
-                // Assign the result to a CustomerEntity object.
-                SingleArtworkEntity = (Artwork)retrievedResult.Result;
+                table.Execute(editOperation);
 
-                //change the properties to new properties
-                //if(SingleArtworkEntity!= null)
-                //{
-                //    SingleArtworkEntity.Title = model.Title,
-                //    SingleArtworkEntity.AddedDate = DateTime.UtcNow,
-                //    SingleArtworkEntity.Artist = model.Artist,
-                //    SingleArtworkEntity.CreationDate = DateTime.UtcNow,
-                //    SingleArtworkEntity.Description = model.Description,
-                //    SingleArtworkEntity.Id = model.Id,
-                //    SingleArtworkEntity.InStorage = model.InStorage,
-                //    SingleArtworkEntity.PartitionKey = "ostra",
-                //    SingleArtworkEntity.RowKey = Identity.ToString(),
-                //    SingleArtworkEntity.Room = model.Room,
-                //    SingleArtworkEntity.ImgUrl = "",
-                //    SingleArtworkEntity.Visible = true,
-                //}                       
+                Console.WriteLine("Entity updated.");
+
             }
 
-
-
-
-            return SingleArtworkEntity;
-
+            else
+            {
+                Console.WriteLine("Entity could not be retrieved.");
+            }
         }
 
 
@@ -190,9 +185,6 @@ namespace MvcArtStone.Repository
 
 
 
-
-
-        
 
 
         public Artwork GetSingleArtworkById(string id)
