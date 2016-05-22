759396ab76831bb039ff664d2eb7ba2e36588a72
filimix <wobzar@hotmail.se>
2016-05-22 13:20:16 +0200

    Search on room works correctly now

--- MvcArtStone/MvcArtStone.Repository/ArtworkRepository.cs
+++ MvcArtStone/MvcArtStone.Repository/ArtworkRepository.cs
@@ -242,7 +242,7 @@ namespace MvcArtStone.Repository
                 //Checks if the REQUIERED Title and Artist are NOT null and then returns a valid query
                 if (artwork.Title != null && artwork.Artist != null)
                 {
-                    queriedEntity = entities.Where(x => x.Title.ToLower().Contains(searchStringToLower) || x.Artist.ToLower().Contains(searchStringToLower));
+                    queriedEntity = entities.Where(x => x.Title.ToLower().Contains(searchStringToLower) || x.Artist.ToLower().Contains(searchStringToLower) || x.Room.ToLower().Contains(searchStringToLower));
                 }
                 //Checks if Title, Artist and Room is not null and then returns a valid query
                 else if (artwork.Title != null && artwork.Artist != null && artwork.Room != null)
